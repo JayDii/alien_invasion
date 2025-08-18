0ce0865cc5f54fce2e981e642edb35c5d0f9e8a2
Jan Decker <jan.decker@student.kit.edu>
2025-08-18 20:25:34 +0200

    Added a framerate clock and background color

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,9 +9,13 @@ class AlienInvasion:
         """Initilize Game and create Game Resources."""
 
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+        # Set background color.
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main Loop for the game."""
@@ -20,8 +24,13 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen each pass through the loop
+            self.screen.fill(self.bg_color)
+
             # Make the most recently draw screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ ==  '__main__':
     # Make game instance, and run the game
